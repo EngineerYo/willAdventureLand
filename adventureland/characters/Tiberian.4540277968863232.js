b37cd1eb0ff59983ad48377ec08e6d4d4952093b
EngineerYo <willcrain96@gmail.com>
2024-01-24 15:59:08 -0800

    Changed character scrips

--- adventureland/characters/Tiberian.4540277968863232.js
+++ adventureland/characters/Tiberian.4540277968863232.js
@@ -2,9 +2,10 @@ const { ranger_loop, give_merchant_loop, draw_loop, form_party } = require_code(
 const { afk_spots } = require_code('98_Constants')
 
 load_code(96)
+load_code(97)
 
 form_party()
 give_merchant_loop()
 
 ranger_loop()
-draw_loop()
+//draw_loop()
