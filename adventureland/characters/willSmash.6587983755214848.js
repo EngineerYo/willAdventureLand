80ff6027d19e7fe7aa4e79f86cc0cd373e1411f3
EngineerYo <willcrain96@gmail.com>
2024-02-01 16:09:51 -0800

    Renamed file from Alanic to WillSmash

---
+++ adventureland/characters/willSmash.6587983755214848.js
@@ -0,0 +1,21 @@
+const { Strategy } = require_code(30)
+let strategy = new Strategy()
+
+strategy.init()
+
+const { give_merchant_loop, form_party } = require_code('21_Loops')
+// const { afk_spots } = require_code('98_Constants')
+
+load_code(96)
+load_code(97)
+
+give_merchant_loop()
+form_party()
+
+// mage_loop()
+
+
+setInterval(() => {
+	if (character.rip && !parent.S.dragold.live) respawn()
+}, 250)
+
